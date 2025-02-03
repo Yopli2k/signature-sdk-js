0cac4d6678b7ae0dc6c71410879f9c8bf6278f44
José Antonio Cuello Principal <yopli2000@gmail.com>
2025-02-03 15:08:30 +0100

    partial commit

--- demos/simple/simple.js
+++ demos/simple/simple.js
@@ -11,24 +11,35 @@ Module.onAbort = _ => {
  	document.getElementById("initializeBanground").style.display = "none";
 }
 
-Module.onRuntimeInitialized = _ => {		
+Module.onRuntimeInitialized = _ => {
     document.getElementById("version_txt").innerHTML = Module.VERSION;
-    mSigObj = new Module.SigObj();	
-	mHash = new Module.Hash(Module.HashType.SHA512);	  
-	
+    mSigObj = new Module.SigObj();
+	mHash = new Module.Hash(Module.HashType.SHA512);
+
 	// Here we need to set the licence. The easiest way is directly using
 	// const promise = mSigObj.setLicence(key, secret);
 	// however here the problem it is that we expose the key and secret publically.
-	// if we want to hide the licence we can get the licence from an external server.				
+	// if we want to hide the licence we can get the licence from an external server.
 	// there is a php demo file in /common/licence_proxy.php
     //const promise = mSigObj.setLicenceProxy("url from where to get the licence");
-	const promise = mSigObj.setLicence("key", "secret");
+	const key = "3c5427a8-b048-4972-b1a3-a297a7311f95";
+	const secret = "nGAtVHhXiydiiUxkqCDye2bq7Gh+M7k+zymCa3kn5OuJiO5xj1XNq0eawd89YUWwNzdGhPQRX1YzqDkaZK/PDA==";
+	const promise = mSigObj.setLicence(key, secret);
+	console.log("Licence set", key, secret);
+	console.log("Promise", promise);
+	document.getElementById("capture_stu_device").disabled = false;
+	document.getElementById("canvas_capture_btn").disabled = false;
+	document.getElementById("initializeBanground").style.display = "none";
+	document.getElementById("myfile").disabled=false;
+
+	/*
 	promise.then(value => {
 	    if (value) {
-	        if (navigator.hid) {				
+	        if (navigator.hid) {
 		        document.getElementById("capture_stu_device").disabled = false;
 		    }
-				
+
+			document.getElementById("capture_stu_device").disabled = false;
 		    document.getElementById("canvas_capture_btn").disabled = false;
 		    document.getElementById("initializeBanground").style.display = "none";
 			document.getElementById("myfile").disabled=false;
@@ -38,12 +49,13 @@ Module.onRuntimeInitialized = _ => {
 		alert(error);
 		document.getElementById("initializeBanground").style.display = "none";
 	});
+    */
 }
 
 async function loadFromFile() {
 	const file = document.getElementById("myfile").files[0];
 	if (file) {
-	  // check the type	  
+	  // check the type
 	  if ("text/plain" == file.type) {
 		  // read the file as string
 		  const reader = new FileReader();
@@ -59,13 +71,13 @@ async function loadFromFile() {
 				alert("Error loading signature as text "+e);
 			}
 		  }
-          reader.readAsText(file);		
+          reader.readAsText(file);
 	  } else if ((file.type == "image/png") ||
-                (file.type == "image/jpeg")) {			  
+                (file.type == "image/jpeg")) {
 		const reader = new FileReader();
         reader.onload = async function() {
           const data = reader.result;
-		  var img = new Image();	     
+		  var img = new Image();
 		  img.addEventListener('load', async function() {
              //the image has been loaded
 			const canvas = document.createElement("canvas");
@@ -74,7 +86,7 @@ async function loadFromFile() {
 			//const ctx = canvas.getContext("2d");
 			//ctx.drawImage(img, 0, 0, img.width, img.height);
 			//const imageData = ctx.getImageData(0, 0, img.width, img.height);
-			
+
 			//Note: using the 2D context, the alpha channel uses premultiplication, so in case we are loading a image
 			//with alpha channel the biometric data embedded inside the image is destroyed, so we use
 			//webgl2 context instead that maintaing the RGBA colors without modification.
@@ -95,11 +107,11 @@ async function loadFromFile() {
 				renderSignature();
 			} catch (e) {
 				alert("Error loading image "+e);
-			}			
+			}
           }, false);
-		  img.src = data;  
+		  img.src = data;
         }
-        reader.readAsDataURL(file);		
+        reader.readAsDataURL(file);
 	  } else {
 		  // we assume is binary data
 		  const reader = new FileReader();
@@ -115,59 +127,59 @@ async function loadFromFile() {
 				alert("Error loading signature as binary "+e);
 			}
 		  }
-          reader.readAsArrayBuffer(file);		
+          reader.readAsArrayBuffer(file);
 	  }
 	}
 }
-	
+
 async function renderSignature() {
 	//pixels = dpi*mm/25.4mm
 	let width = Math.trunc((96*mSigObj.getWidth(false)*0.01)/25.4);
 	let height = Math.trunc((96*mSigObj.getHeight(false)*0.01)/25.4);
-	
+
 	let scaleWidth = 300/width;
 	let scaleHeight = 200/height;
 	let scale = Math.min(scaleWidth, scaleHeight);
-				
+
 	let renderWidth = Math.trunc(width * scale);
 	const renderHeight = Math.trunc(height * scale);
-	
+
 	// render with must be multiple of 4
 	if (renderWidth % 4 != 0) {
 		renderWidth+= renderWidth % 4;
 	}
-	
+
 	let canvas;
 	const inkColor = "#000F55";
-	try {		
-		const image = await mSigObj.renderBitmap(renderWidth, renderHeight, "image/png", 4, inkColor, "white", 0, 0, 0x400000);					
-	    document.getElementById("sig_image").src = image;	
-        document.getElementById("sig_text").value = await mSigObj.getTextData(Module.TextFormat.BASE64);	
+	try {
+		const image = await mSigObj.renderBitmap(renderWidth, renderHeight, "image/png", 4, inkColor, "white", 0, 0, 0x400000);
+	    document.getElementById("sig_image").src = image;
+        document.getElementById("sig_text").value = await mSigObj.getTextData(Module.TextFormat.BASE64);
 	} catch (e) {
 		alert(e);
-	}				
+	}
 }
 
-function captureFromCanvas() {	
+function captureFromCanvas() {
     const config = {};
 	config.source = {mouse:document.getElementById("allow_mouse_check").checked,
-				     touch:document.getElementById("allow_touch_check").checked, 
+				     touch:document.getElementById("allow_touch_check").checked,
 					 pen:document.getElementById("allow_pen_check").checked};
-					 
+
 	const sigCaptDialog = new SigCaptDialog(config);
-	
+
 	sigCaptDialog.addEventListener("ok", function() {
 	    renderSignature();
 	});
-	
+
 	sigCaptDialog.open(mSigObj, null, null, null, null, Module.KeyType.SHA512, mHash);
-	sigCaptDialog.startCapture();			
+	sigCaptDialog.startCapture();
 }
 
 function captureFromSTU() {
     const stuCapDialog = new StuCaptDialog();
 	stuCapDialog.addEventListener("ok", function() {
 	    renderSignature();
-	});				
-	stuCapDialog.open(mSigObj, null, null, null, null, Module.KeyType.SHA512, mHash);					
+	});
+	stuCapDialog.open(mSigObj, null, null, null, null, Module.KeyType.SHA512, mHash);
 }
